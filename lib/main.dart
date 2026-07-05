6bcfe1b06690193236c2c225ac15b70bb0a30ea6
Leonel Espinoza Sotelo <leonelantonio569@gmail.com>
2026-07-05 12:15:00 -0400

    feat: force portrait orientation

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,14 @@ import 'package:brewery_forest/app/application.dart';
 import 'package:brewery_forest/core/di/injection.dart';
 import 'package:brewery_forest/core/env.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:mapbox_maps_flutter/mapbox_maps_flutter.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   MapboxOptions.setAccessToken(Env.mapboxAccessToken);
+
+  await SystemChrome.setPreferredOrientations([.portraitUp, .portraitDown]);
 
   setupDI();
   runApp(const Application());
